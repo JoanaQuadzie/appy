d77ca0e0043fdfbcb4b58a6cf93623bc3a71981c
JoanaQuadzie <joanaq03@gmail.com>
2024-02-12 10:02:17 +0000

    Work on Appy Navbar

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var firstname = "Joana"; // firstname is a word
+var lastName = "Quadzi";
+var age = 9; // age is a number
+
+// let fullName = firstname + " lastName;
+let fullName = `${firstName} ${lastName}`;
+let a = 56;
+let b = 43;
+
+let result = `${a} + ${b} = ${a+b}`;
+console.log(fullName);
+console.log(result);
+
+const boilingwatertempearture = 100;
+
+let areYouHappy = true;
+
+let phone = {color: "black", makeCall: null};
+
+// let phone = {color: "black", makeCall: null}
+if(age == 18) {
+    console.log("welcome to XXX"); 
+}else {
+    console.log("this page is not for you!");
+}
+
+for (let i = 1; i == 8; i++) (
+    console.log("The value if i is", i)
+)
+
